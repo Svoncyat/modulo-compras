8e11fe249002744c98c7fed87e5b4c269608a87d
EfrainDS3 <santiagotorresmurrieta@gmail.com>
2024-10-06 16:26:40 -0500

    Fue agregada la clss comprador

--- src/main/java/com/compras/model/Comprador.java
+++ src/main/java/com/compras/model/Comprador.java
@@ -1,5 +1,20 @@
 package com.compras.model;
 
 public class Comprador {
+
+        private String id;
+        private String nombre;
+    
+        public Comprador(String id, String nombre) {
+            this.id = id;
+            this.nombre = nombre;
+        }
+    
+        public String getId() { 
+            return id; 
+        }
+        public String getNombre() { 
+            return nombre;
+        }
     
 }
