8efc5e9aa9e9edf70f747ae69bb4ca8493738f87
Svoncyat <svoncyat@gmail.com>
2024-10-06 16:17:07 -0500

    Co-authored-by: EfrainDs3 <EfrainDs3@users.noreply.github.com> Co-authored-by: KevinAyachi <KevinAyachi@users.noreply.github.com>

--- src/main/java/com/compras/db/ConexionDB.java
+++ src/main/java/com/compras/db/ConexionDB.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConexionDB {
 
-    private static final String url = "jdbc:sqlserver://localhost:1433;"
+    private static final String url = "jdbc:sqlserver://26.201.13.126:1433;"
             + "database=BD_ModuloCompra;"
             + "user=sa;"
             + "password=5284;"
@@ -23,4 +23,4 @@ public class ConexionDB {
         }
         return con;
     }
-}
+}//hola amiguitos
--- src/main/java/com/compras/model/Articulo.java
+++ src/main/java/com/compras/model/Articulo.java
@@ -1,5 +1,31 @@
 package com.compras.model;
 
 public class Articulo {
+
+    protected  String codigo;
+    protected  String nombre;
+    protected  int stock;
     
+    public Articulo(String codigo, String nombre, int stock) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.stock = stock;
+    }
+
+    
+    public void actualizarStock(int cantidad) {
+        this.stock += cantidad;
+    }
+
+    
+    public String getCodigo() { 
+        return codigo;
+     }
+    public String getNombre() { 
+        return nombre; 
+     }
+    public int getStock() { 
+        return stock; 
+     }
 }
+    
